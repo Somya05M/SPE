30e88e7f5ed7a40efbdaa7d4122a65b1c12bba10
Somya Malgudi <Somya.Malgudi@iiitb.ac.in>
2024-01-23 09:27:46 +0530

    modified x.cpp

--- x.cpp
+++ x.cpp
@@ -1,9 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    cout<<"hello\n"<<" ";
-    int x = 5;
-    int f = 6;
-    cout<<x+f;
-   return 0
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+
+    // Obtain an iterator pointing to the third element
+    auto it = vec.begin() ;
+
+    // Use std::next to get an iterator pointing to the next element
+    auto nextIt = std::next(it);
+
+    // Print the value of the next element
+    std::cout << "Next element: " << *nextIt << std::endl;
+
+    return 0;
 }
